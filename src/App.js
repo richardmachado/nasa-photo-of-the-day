4a59d2b0f5ff0ceee27ce53573664293eb9aff79
Lambda_School_Loaner_198 <lambda_school_loaner_198@Lambda-School-Loaner-198s-MacBook-Pro.local>
2019-11-21 20:21:13 -0800

    refactored to use Hooks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Data from "./data";
-import PreLoadScreen from "./components/PreLoadScreen";
+
 
 function App() {
   return (
     <div className="App">
-      <PreLoadScreen />
+   
       <Data />
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,83 +1,25 @@
-import React, { Component } from "react";
-import axios from "axios";
-import Cards from "./components/Cards";
-import DatePicker from "react-datepicker";
-var moment = require("moment");
-const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-const begin = moment().utcOffset(timezone)._d;
-const max = moment().utcOffset(timezone)._d;
-const currentTime = moment()
-  .utcOffset(timezone)
-  .format();
+import React, {useState, useEffect} from "react";
+import axios from "axios"
+import "./App.css";
+import PhotoCard from "./components/PhotoCard"
 
-export default class Data extends Component {
-  // Holds State
-  constructor(props) {
-    super(props);
-    this.state = {
-      max: max,
-      begin: begin,
-      startDate: currentTime,
-      data: null
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+function App() {
+  const [nasaImg, setNasaImg] = useState([]);
+  useEffect(() => {
+    
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=X7831OHO7jNbCUFp6ZquUbFjI2txHRDvsbay1fU4")
+    .then(res => {
+      setNasaImg(res.data);
+    });
 
-  // Function Run on Date Change
-  handleChange(date) {
-    this.setState(
-      {
-        startDate: date,
-        begin: date
-      },
-      () => {}
-    );
-    setTimeout(
-      axios
-        .get(
-          `https://api.nasa.gov/planetary/apod?api_key=X7831OHO7jNbCUFp6ZquUbFjI2txHRDvsbay1fU4&date=${
-            this.state.startDate === "2019-07-17"
-              ? this.state.startDate.toISOString().slice(0, -14)
-              : date.toISOString().slice(0, -14)
-          }`
-        )
-        .then(response => {
-          this.setState({
-            data: [response.data]
-          });
-        }),
-      3000
-    );
-  }
+  }, []);
 
-  // Initial Data
-  componentDidMount() {
-    axios
-      .get(
-        `https://api.nasa.gov/planetary/apod?api_key=X7831OHO7jNbCUFp6ZquUbFjI2txHRDvsbay1fU4&date=${this.state.startDate.slice(
-          0,
-          -15
-        )}`
-      )
-      .then(response => {
-        this.setState({
-          data: [response.data]
-        });
-      });
-  }
-
-  render() {
-    return (
-      <>
-        <h1>Select Date:</h1>
-        {/* Displays Calendar */}
-        <DatePicker
-          maxDate={this.state.max}
-          selected={this.state.begin}
-          onChange={this.handleChange}
-        />
-        <Cards data={this.state.data} />
-      </>
-    );
-  }
+  return (
+    <div className="App">
+      <header><h1>NASA PHOTO OF THE DAY</h1></header>
+      <PhotoCard title={nasaImg.title} url={nasaImg.url} explanation={nasaImg.explanation} date={nasaImg.date} /> 
+    </div>
+  );
 }
+
+export default App;
