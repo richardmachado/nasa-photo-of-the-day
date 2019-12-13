f60117258fb5692572d0aeea8c3f09c0a6c301f2
Lambda_School_Loaner_198 <lambda_school_loaner_198@Lambda-School-Loaner-198s-MacBook-Pro.local>
2019-12-12 16:31:50 -0800

    added some styling

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,22 +1,55 @@
 import React from "react";
+import styled from 'styled-components';
+
+const  Explanation = styled.p `
+    color: white;
+    font-size: 24px;
+	width: 80%;
+    text-align: center; 
+    font-style: italic;
+`
+
+const Title = styled.h1 `
+    color:white;
+    background-color: blue;
+    text-align: center;
+    animation:blinkingText 2s infinite;
+}
+@keyframes blinkingText{
+    0%{     color: #000;    }
+    49%{    color: red; }
+    50%{    color: orange; }
+    99%{    color: transparent;  }
+    100%{   color: #000;    }
+}
+`
+const Date = styled.h2`
+    color:white
+`
+const Details= styled.h3`
+color: white;
+font-size: 6rem;
+
+
+`
 
 const PhotoCard = (props) => {
     return(
         <div>
-            <h1>
+            <Title>
                 {props.title}
-            </h1>
-            <h2>
-                {props.date}
-            </h2>
-            <img src = {props.url} alt="NASA POTD"/>
+            </Title>
+            <Date>
+                Today's date: {props.date}
+            </Date>
+            <img src = {props.url} alt="img"/>
         <div>
-            <h3>
+            <Details>
                 Details
-            </h3>
-            <p>
+            </Details>
+            <Explanation>
             {props.explanation}
-            </p>
+            </Explanation>
         </div>
         </div>
     );
--- src/data.js
+++ src/data.js
@@ -1,7 +1,20 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios"
-import "./App.css";
+import "./App.scss";
 import PhotoCard from "./components/PhotoCard"
+import styled from 'styled-components';
+
+const Headerfont = styled.div`
+color: white;
+
+text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9,
+    0 5px 0 #aaa, 0 6px 1px rgba(0, 0, 0, 0.1), 0 0 5px rgba(0, 0, 0, 0.1),
+    0 1px 3px rgba(0, 0, 0, 0.3), 0 3px 5px rgba(0, 0, 0, 0.2),
+    0 5px 10px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.2),
+    0 20px 20px rgba(0, 0, 0, 0.15);
+`;
+
+
 
 function App() {
   const [nasaImg, setNasaImg] = useState([]);
@@ -16,7 +29,11 @@ function App() {
 
   return (
     <div className="App">
-      <header><h1>NASA PHOTO OF THE DAY</h1></header>
+      <Headerfont>
+      <div class="center">
+        <h1>NASA PHOTO OF THE DAY</h1>
+        </div>
+        </Headerfont>
       <PhotoCard title={nasaImg.title} url={nasaImg.url} explanation={nasaImg.explanation} date={nasaImg.date} /> 
     </div>
   );
